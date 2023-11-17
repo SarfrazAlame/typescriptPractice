92f60d7a33981f5b3745f121ac399e678ba2753a
Sarfraz Alam <sarfrazk7858@gmail.com>
2023-11-17 17:46:09 +0530

    week8

---
+++ week-8/index.js
@@ -0,0 +1,5 @@
+function getFirstNumber(arr) {
+    return arr[0];
+}
+var ans = getFirstNumber([1, 2, 3]);
+console.log(ans);
---
+++ week-8/index.ts
@@ -0,0 +1,9 @@
+
+type NumberArr = number[]
+
+function getFirstNumber(arr: NumberArr) {
+    return arr[0]
+}
+
+const ans = getFirstNumber([1, 2, 3])
+console.log(ans)
